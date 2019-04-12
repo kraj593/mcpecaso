55340d75f284122b180caca194bfda246c4c009b
Kaushik <kraj593@gmail.com>
2019-04-12 16:25:37 -0400

    fixed two_stage_dfba

--- tsdyssco/core/two_stage_dfba.py
+++ tsdyssco/core/two_stage_dfba.py
@@ -1,6 +1,4 @@
-import cameo
 import numpy as np
-import pandas as pd
 from scipy.integrate import odeint
 
 
@@ -44,17 +42,17 @@ def one_stage_timecourse(initial_concs, time, fluxes):
         time is a timepoint vector
         fluxes is a vector containing flux data for biomass, substrate and products respectively"""
 
-    (data, full_output) = odeint(dfba_fun, initial_concs, time, args=tuple([fluxes]), full_output=1)
+    (data, full_output) = odeint(dfba_fun, initial_concs, time, args=tuple([fluxes]), full_output=True)
     data, time = crop_dfba_timecourse_data(data, time)
     return data.transpose(), time
 
 
 def two_stage_timecourse(initial_concs, time_end, time_switch, two_stage_fluxes):
 
-    """This function generates two_stage dFBA data
-       y0 is a vector containing initial concentrations
-       t_end is the batch end time
-       t_switch is the time at which the second stage becomes active
+    """This function generates two_stage timecourse data using dfba given flux vectors for the two stages
+       initial_concs is a vector containing initial concentrations
+       time_end is the batch end time
+       time_switch is the time at which the second stage becomes active
        Ensure t_switch < t_end
        two_stage_fluxes is a list of two lists that has flux data for biomass, substrate and product respectively"""
     
@@ -64,29 +62,27 @@ def two_stage_timecourse(initial_concs, time_end, time_switch, two_stage_fluxes)
     data_stage_two = []
     stage_one_start_data = initial_concs
     
-    #These two conditions are to ensure that the optimizer functions properly
+    # These two conditions are to ensure that the optimizer functions properly
     if time_switch > time_end:
-        time_end = time_switch
+        time_switch = time_end
     if time_switch < 0:
         time_switch = 0
-        
-    
-    if int(num_of_points*(t_switch/t_end)) != 0:
-        t_stage_one = np.linspace(0,t_switch,int(num_of_points*(t_switch/t_end)))
-        data_stage_one, t_stage_one = generate_timecourse_data(stage_one_start_data, t_stage_one, stage_one_fluxes)
-    else:
-        data_stage_one, t_stage_one = generate_timecourse_data(stage_one_start_data, [0], stage_one_fluxes)
-    
-    
-    stage_two_start_data = data_stage_one.transpose()[-1]
-    if (stage_two_start_data[1] > 0) and (int(num_of_points*(t_end - t_switch)/t_end) != 0):
-        t_stage_two = np.linspace(t_switch, t_end, int(num_of_points*(t_end - t_switch)/t_end))
-        data_stage_two, t_stage_two = generate_timecourse_data(stage_two_start_data, t_stage_two, stage_two_fluxes)
-    else:
-        data_stage_two, t_stage_two = generate_timecourse_data(stage_two_start_data, [t_stage_one[-1]], stage_two_fluxes)
-    
-    data = np.concatenate((data_stage_one, data_stage_two), axis = 1)
-    t = np.concatenate((t_stage_one, t_stage_two), axis = 0)
 
-    return data, t
+    if int(num_of_points*(time_switch/time_end)) != 0:
+        time_stage_one = np.linspace(0, time_switch, int(num_of_points*(time_switch/time_end)))
+        data_stage_one, time_stage_one = one_stage_timecourse(stage_one_start_data, time_stage_one, stage_one_fluxes)
+    else:
+        data_stage_one, time_stage_one = one_stage_timecourse(stage_one_start_data, [0], stage_one_fluxes)
+
+    stage_two_start_data = data_stage_one.transpose()[-1]
+    if (stage_two_start_data[1] > 0) and (int(num_of_points*(time_end - time_switch)/time_end) != 0):
+        time_stage_two = np.linspace(time_switch, time_end, int(num_of_points*(time_end - time_switch)/time_end))
+        data_stage_two, t_stage_two = one_stage_timecourse(stage_two_start_data, time_stage_two, stage_two_fluxes)
+    else:
+        data_stage_two, t_stage_two = one_stage_timecourse(stage_two_start_data, [time_stage_one[-1]], stage_two_fluxes)
+    
+    two_stage_data = np.concatenate((data_stage_one, data_stage_two), axis=1)
+    time = np.concatenate((time_stage_one, t_stage_two), axis=0)
+
+    return two_stage_data, time
 
