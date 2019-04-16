d594d1cc65f7661cfdc73558cc08a984c47e681d
Kaushik <kraj593@gmail.com>
2019-04-16 11:23:40 -0400

    changed variable names for clarity

--- tsdyssco/core/fermentation_metrics.py
+++ tsdyssco/core/fermentation_metrics.py
@@ -1,27 +1,27 @@
-def batch_productivity(dfba_data, time_end):
+def batch_productivity(dfba_data, time):
     """ This function returns the productivity of a batch.
         Input dfba_data should be in the order [biomass, substrate, product]"""
 
-    return dfba_data[2, -1] / time_end[-1]
+    return dfba_data[2, -1] / time[-1]
 
 
-def batch_yield(dfba_data, time_end):
+def batch_yield(dfba_data, time):
     """ This function returns the yield of a batch.
         Input dfba_data should be in the order [biomass, substrate, product]"""
 
     return (dfba_data[2, -1] - dfba_data[2, 0]) / (dfba_data[1, 0] - dfba_data[1, -1])
 
 
-def batch_end_titer(dfba_data, time_end):
+def batch_end_titer(dfba_data, time):
     """ This function returns the end titer of a batch.
         Input dfba_data should be in the order [biomass, substrate, product]"""
 
     return dfba_data[2, -1]
 
 
-def dupont_metric(dfba_data, time_end):
+def dupont_metric(dfba_data, time):
     """ This function returns the approximate dupont metric of a batch.
         Input dfba_data should be in the order [biomass, substrate, product]"""
 
-    return batch_productivity(dfba_data, time_end)*(1-1/batch_yield(dfba_data, time_end))
+    return batch_productivity(dfba_data, time)*(1-1/batch_yield(dfba_data, time))
 
