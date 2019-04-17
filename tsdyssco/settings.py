9a01cc7119a0f59d43522f03da35c14abc45485c
Kaushik <kraj593@gmail.com>
2019-04-16 21:12:03 -0400

    initialized settings as a class

--- tsdyssco/settings.py
+++ tsdyssco/settings.py
@@ -1,9 +1,13 @@
-substrate_uptake = 'generalized_logistic'
-k_m = 0
-parallel = False
-num_points = 50
-objective = 'productivity'
-initial_biomass = 0.05
-initial_substrate = 50
-initial_product = 0
-time_end = 20
+class Settings:
+    def __init__(self):
+        self.k_m = 0
+        self.substrate_uptake = 'generalized_logistic'
+        self.parallel = False
+        self.num_points = 50
+        self.objective = 'productivity'
+        self.initial_biomass = 0.05
+        self.initial_substrate = 50
+        self.initial_product = 0
+        self.time_end = 20
+
+settings = Settings()
