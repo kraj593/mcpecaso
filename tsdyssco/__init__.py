158dfaf807beb52901267ea12cd6694529f6fbc1
Kaushik <kraj593@gmail.com>
2019-04-17 00:56:39 -0400

    code cleanup

--- tsdyssco/__init__.py
+++ tsdyssco/__init__.py
@@ -2,9 +2,7 @@ __author__ = 'Kaushik Raj & Naveen Venayak'
 
 import sys
 from warnings import warn
+from .core import *
 
 if sys.version_info.major < 3:
     warn(Exception('Require Python >= 3.5'))
-
-from .core import *
-
