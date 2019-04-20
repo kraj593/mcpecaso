c462c0da64fc9e71ba25e1e3ed819aeeaf6a001b
Kaushik <kraj593@gmail.com>
2019-04-20 09:51:49 -0400

    formatted axis titles

--- tsdyssco/plotting.py
+++ tsdyssco/plotting.py
@@ -123,12 +123,14 @@ def multiplot_envelopes(dyssco_list):
                     fig['layout']['xaxis' + str(row*num_of_conditions+col+1)]['dtick'] = np.around(max_growth / 5, 2)
 
                     if row == 0:
-                        fig['layout']['yaxis' + str(row*num_of_conditions+1)]['title'] = 'Substrate Uptake (mmol/gdw.h)'
+                        fig['layout']['yaxis' + str(row*num_of_conditions+1)]['title'] = 'Substrate Uptake<br>' \
+                                                                                         '(mmol/gdw.h)'
                         fig['layout']['xaxis'+str(row*num_of_conditions+col+1)]['title'] = 'Growth Rate (1/h)'
                         fig['layout']['yaxis'+str(row*num_of_conditions+col+1)]['range'] = [0, 1.2*max_uptake]
 
                     if row == 1:
-                        fig['layout']['yaxis' + str(row*num_of_conditions+1)]['title'] = 'Product Flux (mmol/gdw.h)'
+                        fig['layout']['yaxis' + str(row*num_of_conditions+1)]['title'] = 'Product Flux<br>' \
+                                                                                         '(mmol/gdw.h)'
                         fig['layout']['xaxis'+str(row*num_of_conditions+col+1)]['title'] = 'Growth Rate (1/h)'
                         fig['layout']['yaxis'+str(row*num_of_conditions+col+1)]['range'] = [0, 1.2*max_flux]
 
