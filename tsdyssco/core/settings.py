8b2eca8adc390433209f710561d29921a3183f9c
Kaushik <kraj593@gmail.com>
2019-04-23 00:56:26 -0400

    fixed issues with parallelizing code with settings

--- tsdyssco/core/settings.py
+++ tsdyssco/core/settings.py
@@ -3,12 +3,12 @@ class Settings:
         self.k_m = 0
         self.substrate_uptake = 'generalized_logistic'
         self.parallel = False
-        self.num_points = 50
+        self.num_points = 25
         self.objective = 'batch_productivity'
         self.initial_biomass = 0.05
         self.initial_substrate = 50
         self.initial_product = 0
-        self.time_end = 20
+        self.time_end = 100
         self.productivity_coefficient = 0
         self.yield_coefficient = 0
         self.titer_coefficient = 0
