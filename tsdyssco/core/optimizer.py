d714de90fda7214704fc6e6b47668b1b8e6d53c9
Kaushik <kraj593@gmail.com>
2019-04-23 00:55:11 -0400

    fixed issues with parallelizing code, changed optimizer to COBYLA - seems to work best

--- tsdyssco/core/optimizer.py
+++ tsdyssco/core/optimizer.py
@@ -1,25 +1,27 @@
-from scipy.optimize import minimize_scalar
+from scipy.optimize import minimize
 from .fermentation_metrics import *
 from.two_stage_dfba import *
 
 
-def optimization_target(time_switch, initial_concentrations, time_end, two_stage_fluxes, objective_fun):
-    data, time = two_stage_timecourse(initial_concentrations, time_end, time_switch, two_stage_fluxes)
-    return -objective_fun(data, time)
+def optimization_target(time_switch, initial_concentrations, time_end, two_stage_fluxes, objective_fun, settings):
+    data, time = two_stage_timecourse(initial_concentrations, time_end, time_switch, two_stage_fluxes,
+                                      settings.num_timepoints)
+    return -objective_fun(data, time, settings)
 
 
-def optimal_switch_time(initial_concentrations, time_end, two_stage_fluxes, objective_fun=batch_productivity):
+def optimal_switch_time(initial_concentrations, time_end, two_stage_fluxes, settings, objective_fun=batch_productivity):
 
-    opt_result = minimize_scalar(optimization_target,
-                                 args=(initial_concentrations, time_end, two_stage_fluxes, objective_fun),
-                                 bounds=[0, time_end], options={'maxiter': 1000000}, method='brent')
+    opt_result = minimize(optimization_target, x0=np.array([4]),
+                          args=(initial_concentrations, time_end, two_stage_fluxes, objective_fun, settings),
+                          options={'maxiter': 1000000}, method='COBYLA')
 
-    temp_data, temp_time = two_stage_timecourse(initial_concentrations, time_end, opt_result.x, two_stage_fluxes)
+    temp_data, temp_time = two_stage_timecourse(initial_concentrations, time_end, opt_result.x, two_stage_fluxes,
+                                                settings.num_timepoints)
 
-    if opt_result.x <= 0:
-        opt_result.x = 0
-    elif opt_result.x > temp_time[-1]:
-        opt_result.x = temp_time[-1]
+    if opt_result.x[0] <= 0:
+        opt_result.x[0] = 0
+    elif opt_result.x[0] > temp_time[-1]:
+        opt_result.x[0] = temp_time[-1]
     return opt_result
 
 
