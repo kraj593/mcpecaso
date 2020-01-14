875b41f84e085478465e04af67294c884137634c
Kaushik <kraj593@gmail.com>
2020-01-13 19:59:14 -0500

    removes ability to calculate dupont metric

--- tsdyssco/core/fermentation_metrics.py
+++ tsdyssco/core/fermentation_metrics.py
@@ -24,16 +24,6 @@ def batch_end_titer(dfba_data, time, settings):
     return dfba_data[2, -1]
 
 
-def dupont_metric(dfba_data, time, settings):
-    """ This function returns the approximate dupont metric of a batch.
-        Input dfba_data should be in the order [biomass, substrate, product]"""
-    yd = batch_yield(dfba_data, time, settings)
-    if yd > 0:
-        return batch_productivity(dfba_data, time, settings)*(1-1/yd)
-    else:
-        return 0
-
-
 def linear_combination(dfba_data, time, settings):
     """ This function returns a linear combination of the above defined metrics in case it is needed as an
         objective function. Input dfba_data should be in the order [biomass, substrate, product]"""
