5d25fdbd4a13dd2bbfcae285a22a83056f8ff697
Kaushik <kraj593@gmail.com>
2020-01-13 22:24:12 -0500

    adds ability to give constraints to the optimizer. Constraints on productivity, yield and titer are supported for now.

--- tsdyssco/core/optimizer.py
+++ tsdyssco/core/optimizer.py
@@ -3,14 +3,26 @@ from .fermentation_metrics import *
 from.two_stage_dfba import *
 
 
-def constraint_function(time_switch, min_productivity, min_yield, min_titer, initial_concentrations,
-                        time_end, two_stage_fluxes, settings):
+def productivity_constraint(time_switch, min_productivity, initial_concentrations,
+                            time_end, two_stage_fluxes, settings):
     data, time = two_stage_timecourse(initial_concentrations, time_end, *list(time_switch), two_stage_fluxes,
                                       settings.num_timepoints)
 
-    return ((batch_productivity(data, time, settings) - min_productivity) *
-            (batch_yield(data, time, settings) - min_yield) *
-            (batch_end_titer(data, time, settings) - min_titer))
+    return batch_productivity(data, time, settings) - min_productivity
+
+
+def yield_constraint(time_switch, min_yield, initial_concentrations, time_end, two_stage_fluxes, settings):
+    data, time = two_stage_timecourse(initial_concentrations, time_end, *list(time_switch), two_stage_fluxes,
+                                      settings.num_timepoints)
+
+    return batch_yield(data, time, settings) - min_yield
+
+
+def titer_constraint(time_switch, min_titer, initial_concentrations, time_end, two_stage_fluxes, settings):
+    data, time = two_stage_timecourse(initial_concentrations, time_end, *list(time_switch), two_stage_fluxes,
+                                      settings.num_timepoints)
+
+    return batch_end_titer(data, time, settings) - min_titer
 
 
 def optimization_target(time_switch, initial_concentrations, time_end, two_stage_fluxes, objective_fun, settings):
@@ -21,14 +33,27 @@ def optimization_target(time_switch, initial_concentrations, time_end, two_stage
 
 
 def optimal_switch_time(initial_concentrations, time_end, two_stage_fluxes, settings,
-                        min_productivity=0, min_yield=0, min_titer=0, objective_fun=batch_productivity):
+                        objective_fun=batch_productivity, min_productivity=0, min_yield=0, min_titer=0):
+
+    constraints = []
+
+    if min_productivity:
+        constraints.append({'type': 'ineq', 'fun': productivity_constraint,
+                            'args': ([min_productivity, initial_concentrations, time_end, two_stage_fluxes, settings])})
+
+    if min_yield:
+        constraints.append({'type': 'ineq', 'fun': yield_constraint,
+                            'args': ([min_yield, initial_concentrations, time_end, two_stage_fluxes, settings])})
+
+    if min_titer:
+        constraints.append({'type': 'ineq', 'fun': titer_constraint,
+                            'args': ([min_titer, initial_concentrations, time_end, two_stage_fluxes, settings])})
 
     opt_result = minimize(optimization_target, x0=np.array([4]),
                           args=(initial_concentrations, time_end, two_stage_fluxes, objective_fun, settings),
                           options={'maxiter': 1000000}, method='COBYLA',
-                          constraints={'type': 'ineq', 'fun': constraint_function,
-                                       'args': ([min_productivity, min_yield, min_titer, initial_concentrations,
-                                                time_end, two_stage_fluxes, settings])})
+                          constraints=constraints
+                          )
 
     temp_data, temp_time = two_stage_timecourse(initial_concentrations, time_end, opt_result.x[0], two_stage_fluxes,
                                                 settings.num_timepoints)
